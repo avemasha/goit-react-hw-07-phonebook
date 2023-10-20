00e340daef80dec3bed469b78d754fdbc4428efc
Maria Novosad <100295231+avemasha@users.noreply.github.com>
2023-10-20 18:30:31 +0200

    state

--- src/components/store/contactsSlice.js
+++ src/components/store/contactsSlice.js
@@ -1,28 +1,11 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 export const contactInitialState = {
-  contacts: [
-    {
-      text: { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      id: 1,
-      completed: false,
-    },
-    {
-      text: { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      id: 2,
-      completed: false,
-    },
-    {
-      text: { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      id: 3,
-      completed: false,
-    },
-    {
-      text: { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      id: 4,
-      completed: false,
-    },
-  ],
-  filter: '',
+  contacts: {
+    items: [],
+    isLoading: false,
+    error: null
+  },
+  filter: ""
 };
 
 const contactsSlice = createSlice({
